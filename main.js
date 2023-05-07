2a5b3474d34cd07997eaae06debfdac162b636dc
Derek Truesdale <dtruesdale37@gmail.com>
2023-05-07 15:15:42 -0500

    lab completed

--- main.js
+++ main.js
@@ -3,6 +3,46 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+let modal = document.getElementById('modal')
+modal.className = 'hidden'
+
+document.addEventListener('DOMContentLoaded',()=>{
+  const likeGlyphs= document.querySelectorAll('.like-glyph')
+  console.log(likeGlyphs)
+  likeGlyphs.forEach((glyph)=>{
+    glyph.addEventListener('click',(e)=>{
+      // e.preventDefault()
+      mimicServerCall()
+      .then(() => {
+        changeHeart((e.target))
+      })
+      .catch(()=>{
+        modal.className =""
+       setTimeout(handleModal, 3000)
+      })
+    })
+  })
+
+})
+
+function changeHeart(heartValue){
+  console.log('heartValue',heartValue)
+  if(heartValue.innerText === EMPTY_HEART){
+    heartValue.innerText = FULL_HEART
+    heartValue.className ='activated-heart'
+    console.log('heart1',heartValue)
+  } else if(heartValue.innerText === FULL_HEART){
+  heartValue.innerText = EMPTY_HEART
+  heartValue.className = ''
+  console.log('heart2',heartValue)
+  }
+}
+
+function handleModal(){
+  console.log('modal', modal)
+  modal.className = 'hidden'
+
+}
 
 
 
